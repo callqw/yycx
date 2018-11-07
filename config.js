6e0c75b005c4346df7ec8c6fa53ef650b8025851
402555290 <402555290@qq.com>
2018-11-07 18:16:46 +0800

    no message

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const IS_DEV = process.env.NODE_ENV === 'development'
-exports.API_HOST = IS_DEV ? 'localhost' : 'localhost'
+exports.API_HOST = IS_DEV ? '127.0.0.1' : '127.0.0.1'
 exports.API_PORT = IS_DEV ? '8000' : '3000'
 
 // exports.STATIC_PATH = 'https://static.jkchao.cn'
