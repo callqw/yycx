4683591c48ea2ebaa4edaeb5899eb96971ea47d7
402555290 <402555290@qq.com>
2018-11-08 11:55:19 +0800

    no message

---
+++ pages/company.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+            <!--<el-button icon="back" slot="left">返回</el-button>-->
+            <!--<el-button icon="more" slot="right">asdf</el-button>-->
+        <h1 >demo</h1>
+    </div>
+
+</template>
+<script>
+    export default {
+        head(){
+            return {
+                title: '公司介绍',
+                meta: [
+                    { hid: 'description', name: 'description', content: '公司介绍' }
+                ],
+
+            }
+        },
+    }
+</script>
+<style lang="scss">
+
+</style>
