eb49f063990883d884b055d8aed8525490e286ad
402555290 <402555290@qq.com>
2018-11-12 20:51:53 +0800

    no message

---
+++ pages/callUs.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+            <!--<el-button icon="back" slot="left">返回</el-button>-->
+            <!--<el-button icon="more" slot="right">asdf</el-button>-->
+        <h1 >demo</h1>
+    </div>
+
+</template>
+<script>
+    export default {
+        head(){
+            return {
+                title: '联系我们',
+                meta: [
+                    { hid: 'description', name: 'description', content: '联系我们' }
+                ],
+
+            }
+        },
+    }
+</script>
+<style lang="scss">
+
+</style>
---
+++ pages/package.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+            <!--<el-button icon="back" slot="left">返回</el-button>-->
+            <!--<el-button icon="more" slot="right">asdf</el-button>-->
+        <el-button >package</el-button>
+    </div>
+
+</template>
+<script>
+    export default {
+    }
+</script>
+<style lang="scss">
+
+</style>
